4018cb43e375f6016e4aa00cab763ad2d8221b06
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-08-19 12:03:35 +0200

    user profile with repos

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos:[],
     loading: false,
     alert: null,
+    
   };
 
   // async componentDidMount() {
@@ -46,6 +48,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    console.log({ data: res.data });
+    this.setState({ repos: res.data, loading: false });
+  };
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -57,7 +68,7 @@ class App extends Component {
   render() {
     console.log({ state: this.state });
 
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -89,6 +100,8 @@ class App extends Component {
                 <User
                   {...props}
                   getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  repos={repos}
                   user={user}
                   loading={loading}
                 />
