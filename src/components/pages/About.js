3fb4f972277b20c2977328b6d82a47abce88edb1
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-08-19 10:47:11 +0200

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    console.log('about render')
+    return (
+        <div>
+            <h1> About App </h1>
+            <p> App to search Github users</p>
+            <p>Version: 1.0</p>
+        </div>
+    )
+}
+
+export default About
