fc97f9876efe9b0bd95d81c7d854b79d2f174598
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-08-22 11:58:04 +0200

    Final commit befor deploying

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,108 +1,104 @@
-import React, { Component } from "react";
+import React, { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
-import Repos from "../repos/Repos"
-import PropTypes from "prop-types";
+import Repos from "../repos/Repos";
 import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
-  }
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos:PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos:PropTypes.func.isRequired,
-  };
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+    company,
+  } = user;
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-      company
-    } = this.props.user;
+  if (loading) return <Spinner />;
 
-    const { loading, repos } = this.props;
-
-    if (loading) return <Spinner />;
-
-    return (
-      <>
-        <Link to="/" className="btn btn-light">
-          Back to Search{" "}
-        </Link>
-        Hireable: {""}
-        {hireable ? (
-          <i className="fas fa-check text-success" />
-        ) : (
-          <i className="fas fa-times-circle text-danger" />
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              className="round-img"
-              alt=""
-              style={{ width: "150px" }}
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <>
-                <h3>Bio:</h3>
-                <p> {bio} </p>
-              </>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              {" "}
-              Visit Github progile
-            </a>
-            <ul>
-                <li>
-                    {login && <>
-                        <strong>Username:</strong> {login}
-                    </>}
-                </li>
-                <li>
-                    {company && <>
-                        <strong>Company:</strong> {company}
-                    </>}
-                </li>
-                <li>
-                    {blog && <>
-                        <strong>Website:</strong> {blog}
-                    </>}
-                </li>
-            </ul>
-          </div>
+  return (
+    <>
+      <Link to="/" className="btn btn-light">
+        Back to Search{" "}
+      </Link>
+      Hireable: {""}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt=""
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
-        <div className="card text-center">
-            <div className="badge badge-primary">Followers: {followers}</div>
-            <div className="badge badge-success">Following: {following}</div>
-            <div className="badge badge-light">Public Repos: {public_repos}</div>
-            <div className="badge badge-dark">Public Gists: {public_gists}</div>
-
+        <div>
+          {bio && (
+            <>
+              <h3>Bio:</h3>
+              <p> {bio} </p>
+            </>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            {" "}
+            Visit Github progile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <>
+                  <strong>Username:</strong> {login}
+                </>
+              )}
+            </li>
+            <li>
+              {company && (
+                <>
+                  <strong>Company:</strong> {company}
+                </>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <>
+                  <strong>Website:</strong> {blog}
+                </>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </>
+  );
+};
 
 export default User;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
-import PropTytpes from 'prop-types'
+import GithubContext from '../../context/github/githubContext'
+import GithubState from "../../context/github/GithubState";
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext)
+
+  const {loading, users} = githubContext
+
   if (loading) {
     return <Spinner />;
   } else {
@@ -17,10 +22,6 @@ const Users = ({ users, loading }) => {
   }
 };
 
-Users.PropTytpes = {
-    users: PropTytpes.array.isRequired,
-    loading: PropTytpes.bool.isRequired,
-}
 
 const userStyle = {
   display: "grid",
